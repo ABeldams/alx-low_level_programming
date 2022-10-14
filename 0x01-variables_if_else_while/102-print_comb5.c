acb2331d7dffa44dab693dd97bf3f8db61b3a8e9
aliyu bello <aliyu.beldam1@gmail.com>
2022-10-14 13:45:49 -0700

    dd

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,13 +3,13 @@
  * main - create 2 pairs of number
  * Return: 0
  */
-int main(void) 
-{  
+int main(void)
+{
 int i, j;   
 for (i = 0; i <= 99; i++)
 {
 for (j = i; j <= 99; j++)
-{ 
+{
 if (j != i)
 {
 putchar(i / 10 + 48);
@@ -24,7 +24,7 @@ putchar(' ');
 }
 }
 }
-} 
+}
 putchar('\n');
 return (0);
 }
