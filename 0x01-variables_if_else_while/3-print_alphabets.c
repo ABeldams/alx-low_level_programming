4e2b5ccd8ff42eb85127c14c71a15803391f0b55
aliyu bello <aliyu.beldam1@gmail.com>
2022-10-14 12:40:40 -0700

    dd

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,20 +5,20 @@
  * and then in uppercase, followed by a new line.
  * Return: 0
  */
-int main(void) 
-{  
+int main(void)
+{
 char c = 'a';
-while (c <= 'z')    
-{      
+while (c <= 'z')
+{
 putchar(c);
-c++;      
-}  
-c = 'A'; 
-while (c <= 'Z')    
-{      
-putchar(c);      
-c++;      
-}  
+c++;
+}
+c = 'A';
+while (c <= 'Z')
+{
+putchar(c);
+c++;
+}
 putchar('\n');
 return (0);
 }
