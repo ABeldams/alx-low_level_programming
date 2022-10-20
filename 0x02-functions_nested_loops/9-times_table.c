a57d3e8f54d9bb9ac798bf54eeb70c60bc55c8df
aliyu bello <aliyu.beldam1@gmail.com>
2022-10-20 14:55:45 -0700

    d

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,36 +1,80 @@
-#include "main.h" 
-/** 
-* times_table -  a function that prints the 9 times table, starting with 0 
-* rone = row, cone = column, d = digits of current result 
-* Return: times table 
-* add extra space past single digit 
-*/
-void times_table(void)
+#include "main.h"
+/**
+ * times_table - Print the 9 times table, starting with 0
+ * Return: 0
+ */
+void times_table(void)
 {
-int rone, cone, d;
-for (rone = 0; rone <= 9; rone++)
-{
-_putchar('0');
-_putchar(',');
-_putchar(' ');
-for (cone = 1; cone <= 9; cone++)
-{
-d = (rone * cone);
-if ((d / 10) > 0)
-{
-_putchar((d / 10) + '0');
-}
-else
-{
-_putchar(' ');
-}
-_putchar((d % 10) + '0');
-if (cone < 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
+  int x = 0; /* factor */
+  
+  int y; /* count  */
+  
+  int z; /* computed value */
+  
+
+  
+  while (x < 10)
+    
+    {
+      
+      y = 0;
+      
+      while (y < 10)
+	
+	{
+	  
+	  z = x * y;
+	  
+
+	  
+	  if (z > 9)
+	    
+	    {
+	      
+	      _putchar(z / 10 + '0');
+	      
+	      _putchar(z % 10 + '0');
+	      
+	    }
+	  
+	  else if (y != 0)
+	    
+	    {
+	      
+	      _putchar(' ');
+	      
+	      _putchar(z + '0');
+	      
+	    }
+	  
+	  else
+	    
+	    {
+	      
+	      _putchar(z + '0');
+	      
+	    }
+	  
+
+	  
+	  if (y != 9)
+	    
+	    {
+	      
+	      _putchar(',');
+	      
+	      _putchar(' ');
+	      
+	    }
+	  
+	  y++;
+	  
+	}
+      
+      _putchar('\n');
+      
+      x++;
+      
+    }
+  
 }
