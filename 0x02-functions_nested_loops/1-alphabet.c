95eaddafc8d59b638b21dc59d7b1a0f2c640e647
aliyu bello <aliyu.beldam1@gmail.com>
2022-10-20 14:29:37 -0700

    ddgt

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,27 @@
-#include "main.h"
-/** 
-* print_alphabet - a function that prints the alphabet, in lowercase 
-* 
-* Return: Always 0 (Succes) 
-*/ 
-void print_alphabet(void)
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+  
 {
-int n;
-for (n = 'a'; n <= 'z'; n++)
-{
-_putchar(n);
-}
-_putchar('\n');
+  
+  int c = 'a';
+  
+
+  
+  while (c <= 'z')
+    
+    {
+      
+      _putchar(c);
+      
+      c++;
+      
+    }
+  
+  _putchar('\n');
+  
 }
