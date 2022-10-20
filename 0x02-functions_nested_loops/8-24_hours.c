521e341551b0ff8f064a29bd1ad07e7fd02f0ae9
aliyu bello <aliyu.beldam1@gmail.com>
2022-10-20 14:52:18 -0700

    dd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,36 @@
-#include "main.h" 
-/** 
-* jack_bauer - function that prints every minute of the day, 24 hour clock 
-* h = hour, m = minutes 
-* / 10 allows second digit to rotate 
-* for loop breaks before passing 24:00
-* Return: 24 hour clock line by line 
-*/ 
-void jack_bauer(void)
+#include "main.h"
+/**
+ * jack_bauer - Day timing
+ *
+ */
+void jack_bauer(void)
 {
-int h, m; 
-for (h = 0; h < 24; h++)
-{
-for (m = 0; m < 60; m++)
-{
-_putchar((h / 10) + '0');
-_putchar((h % 10) + '0');
-_putchar(':');
-_putchar((m / 10) + '0');
-_putchar((m % 10) + '0');
-_putchar('\n');
-}
-}
+  int h, s;
+  
+
+  
+  for (h = 0; h <= 23; h++)
+    
+    {
+      
+      for (s = 0; s <= 59; s++)
+	
+	{
+	  
+	  _putchar((h / 10) + '0');
+	  
+	  _putchar((h % 10) + '0');
+	  
+	  _putchar(':');
+	  
+	  _putchar((s / 10) + '0');
+	  
+	  _putchar((s % 10) + '0');
+	  
+	  _putchar('\n');
+	  
+	}
+      
+    }
+  
 }
