9b3ee8133d618d49670a84592da5beea6ebb8f51
Aliyu Bello <aliyu.beldam1@gmail.com>
2022-10-20 15:09:21 +0100

    kgkgj

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,10 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
-extern int _putchar(char c); 
+#ifndef MAIN
+#define MAIN
+extern int _putchar(char c);
 extern int _isupper(int c);
 extern int _isdigit(int c);
 extern int mul(int a, int b);
-extern void print_numbers(void);
+extern void print_numbers(void); 
 extern void print_most_numbers(void);
 extern void more_numbers(void);
 extern void print_line(int n);
