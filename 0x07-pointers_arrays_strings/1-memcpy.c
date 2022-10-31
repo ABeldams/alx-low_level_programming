8b77d1c4af387ad8215654678b3a56e920c6171e
aliyu bello <aliyu.beldam1@gmail.com>
2022-10-31 04:50:27 -0700

    ddd

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,13 +6,13 @@
  * @n: lenght of buffer
  * Return: new string.
  */
-char *_memcpy(char *dest, char *src, unsigned int n) 
-{  
-unsigned int i = 0;  
-while (i < n)    
-{      
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+while (i < n)
+{
 *(dest + i) = *(src + i);
 i++;
 }
-return (dest); 
+return (dest);
 }
