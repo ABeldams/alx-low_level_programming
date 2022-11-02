b613b6da76996dae459ec2d4624472046bd1bc8d
aliyu bello <aliyu.beldam1@gmail.com>
2022-11-02 05:19:29 -0700

    ddd

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,23 +4,13 @@
  * @s: the pointer input
  *
  */
-
 void _puts_recursion(char *s)
-  
 {
-  
-  if (*s == '\0')
-    
-    {
-      
-      _putchar('\n');
-      
-      return;
-      
-    }
-  
-  _putchar(*s);
-  
-  _puts_recursion(++s);
-  
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(++s);
 }
