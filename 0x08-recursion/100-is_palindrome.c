9515eeb7f38260ed1044ff57bf11e9e7aeb3c636
aliyu bello <aliyu.beldam1@gmail.com>
2022-11-02 16:23:32 -0700

    ddd

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,16 +8,11 @@
 int _strlen_recursion(char *s)
 {
 if (*s)
-    
-    {
-      
-      s++;
-      
-      return (1 + _strlen_recursion(s));
-      
-    }
-  
-  return (0); 
+{
+s++;
+return (1 + _strlen_recursion(s);
+}
+return (0);
 }
 /**
  * checker - helper function for is_palindrome
@@ -28,24 +23,20 @@ if (*s)
  */
 int checker(char *str, int len, int count)
 {
-  
-  if (count >= len)
-    
-    return (1);
-  
-  if (str[len] == str[count])
-    
-    return (checker(str, len - 1, count + 1));
-  return (0); 
+if (count >= len)
+return (1);
+if (str[len] == str[count])
+return (checker(str, len - 1, count + 1));
+return (0);
 }
 /**
  * is_palindrome - checks if the string is a palindrome
  * @s: the string to check
  * Return: 1 if string is a palindrome, 0 if it is not.
  */
-int is_palindrome(char *s) 
+int is_palindrome(char *s)
 {
-  int len = _strlen_recursion(s);
-  int count = 0;
-  return (checker(s, len - 1, count)); 
+int len = _strlen_recursion(s);
+int count = 0;
+return (checker(s, len - 1, count));
 }
