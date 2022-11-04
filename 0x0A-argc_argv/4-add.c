2dfac4a0d616128bb958217d5575c7cb9f9105f5
aliyu bello <aliyu.beldam1@gmail.com>
2022-11-04 06:59:13 -0700

    ddd

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,17 +8,32 @@
  * Return: 0
  */
 int main(int argc, char *argv[])
+  
 {
-int i, sum = 0;
-if (argc < 1)
-return (0);
-for (i = 1; i < argc; i++)
-{      
-if (!atoi(argv[i]))
-{
-printf("%s\n", "Error");
-return (1);
-}
+  
+  int i, sum = 0;
+  
+
+  
+  if (argc < 1)
+    
+    return (0);
+  
+
+  
+  for (i = 1; i < argc; i++)
+    
+    {
+      
+      if (!atoi(argv[i]))
+	
+	{
+	  
+	  printf("%s\n", "Error");
+	  
+	  return (1);
+	  
+	}
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
