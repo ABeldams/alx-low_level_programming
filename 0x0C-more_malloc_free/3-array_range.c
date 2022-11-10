143d2a061bc033066207aeb3cfac6fab5ae3470c
aliyu bello <aliyu.beldam1@gmail.com>
2022-11-10 06:24:28 -0800

    ddd

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,46 +10,23 @@
  */
 int *array_range(int min, int max)
 {
-  
-  int *ptr;
-  
-  int arr;
-  
-
-  
-  if (min > max)
-    
-    {
-      
-      return (NULL);
-      
-    }
-  
-
-  
-  ptr = malloc(sizeof(int) * (max - min + 1));
-  
-  if (ptr == NULL)
-    
-    {
-      
-      return (NULL);
-      
-    }
-  
-  arr = 0;
-  
-  while (min <= max)
-    
-    {
-      
-      ptr[arr] = min;
-      
-      min++;
-      
-      arr++;
-      
-    }
-  
-  return (ptr); 
+int *ptr;
+int arr;
+if (min > max)
+{
+return (NULL);
+}
+ptr = malloc(sizeof(int) * (max - min + 1));
+if (ptr == NULL)
+{
+return (NULL);
+}
+arr = 0;
+while (min <= max)
+{
+ptr[arr] = min;
+min++;
+arr++;
+}
+return (ptr);
 }
