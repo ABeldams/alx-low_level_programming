4e89d727c2bd01504b4b2089a2b31576674313c5
aliyu bello <aliyu.beldam1@gmail.com>
2022-11-10 06:08:58 -0800

    fff

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,21 +1,11 @@
 #include <stdlib.h>
-
-
-
 /**
-
  * _calloc - Allocate memory for array of nmemb elements of size bytes
-
  * @nmemb: Number of elemnts
-
  * @size: Size in bytes of elements
-
  *
-
  * Return: Pointer to new memory, NULL if it fails
-
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
   
 {
