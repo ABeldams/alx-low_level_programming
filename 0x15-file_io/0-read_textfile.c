16d239178800e7df11d761cc59e2c3b70f940d9a
ABeldams <87949987+ABeldams@users.noreply.github.com>
2022-12-05 15:01:12 +0100

    alx

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,71 +1,46 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-
- * read_textfile - reads a text file and prints it
-
- * to the POSIX standard output
-
- * @filename: name of the file
-
- * @letters: number of letters it should read and print
-
- * Return: actual number of letters it could read and print
-
+ * read_textfile - reads a text file and prints it to the POSIX std-output.
+ * @filename: the file to open.
+ * @letters: number of letters it should read and print.
+ * Return: number of letters it could read and print, or 0 if:
+ * - filename is NULL.
+ * - the file can not be opened or read.
+ * - write fails or does not write the expected amount of bytes.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
-  
 {
-  
-  int fd, num_to_print, written_num;
-  
-  char *buffer;
-  
+	int file_descriptor = -1;
+	ssize_t output = 0;
+	char *buffer;
 
-  
-  if (!filename)
-    
-    return (0);
-  
+	if (!filename)
+		return (0);
 
-  
-  fd = open(filename, O_RDONLY);
-  
-  if (fd == -1)
-    
-    return (0);
-  
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor < 0)
+		return (0);
 
-  
-  buffer = malloc(letters * sizeof(char));
-  
-  if (!buffer)
-    
-    return (0);
-  
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(file_descriptor);
+		return (0);
+	}
 
-  
-  /* return Number of bytes read on success */
-  
-  num_to_print = read(fd, buffer, letters);
-  
-  /* return Number of bytes written on success */
-  
-  written_num = write(STDOUT_FILENO, buffer, num_to_print);
-  
+	output = read(file_descriptor, buffer, letters);
+	if (output < 0)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
-  
-  if (written_num < 0)
-    
-    return (0);
-  
-
-  
-  free(buffer);
-  
-  close(fd);
-  
-  return (num_to_print);
-  
+	output = write(STDOUT_FILENO, buffer, output);
+	free(buffer);
+	close(file_descriptor);
+	if (output < 0)
+		return (0);
+	return (output);
 }
